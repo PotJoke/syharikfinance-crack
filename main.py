009399c2832f428f0dc101ada0e79c46dad395e3
PotJoke <bulankov69@gmail.com>
2026-02-27 17:31:13 +0300

    small corrections

--- main.py
+++ main.py
@@ -3,25 +3,27 @@ import urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+auth = "" #YOUR AUTH TOKEN HERE
+cookie = "" #YOUR COOKIE HERE
+
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:148.0) Gecko/20100101 Firefox/148.0",
-    "Accept": "*/*",
-    "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
-    "Accept-Encoding": "gzip, deflate, br, zstd",
-    "Referer": "https://admin107.fvds.ru/admin",
-    "Content-Type": "application/json",
-    "Authorization": "..." #YOUR TOKEN
+    "Cookie": str({cookie}),
+    "Authorization": str({auth})
 }
 
-restart_url = "https://admin107.fvds.ru/api/runs/restart"
-finish_url = "https://admin107.fvds.ru/api/runs/finish"
+url = "" #URL HIDDEN DUE TO COMPETITION RULES
+
+restart_url = f"{url}/api/runs/restart"
+finish_url = f"{url}/api/runs/finish"
+
+scenarioCode = "bike_dream"
 
 restart_data = {
-    "scenarioCode": "bike_dream"
+    "scenarioCode": f"{scenarioCode}"
 }
 
 finish_data = {
-    "scenarioCode": "bike_dream",
+    "scenarioCode": f"{scenarioCode}",
     "status": "passed",
     "finalBudget": 5001,
     "earned": 4001,
@@ -29,7 +31,7 @@ finish_data = {
 }
 
 finish_data2 = {
-    "scenarioCode": "bike_dream",
+    "scenarioCode": f"{scenarioCode}",
     "status": "failed",
     "finalBudget": 4999,
     "earned": 3999,
@@ -40,11 +42,11 @@ try:
     while True:
         response_restart = requests.post(restart_url, headers=headers, json=restart_data, verify=False)
         print(f"RESPONSE: {response_restart.status_code}")
-        print(response_restart.text)
+        #print(response_restart.text)
 
         response_finish = requests.post(finish_url, headers=headers, json=finish_data, verify=False)
         print(f"RESPONSE: {response_finish.status_code}")
-        print(response_finish.text)
+        #print(response_finish.text)
 
         response_restart = requests.post(restart_url, headers=headers, json=restart_data, verify=False)
         print(f"RESPONSE: {response_restart.status_code}")
